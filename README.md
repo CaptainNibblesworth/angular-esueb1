09c9c42034509125229a0a245367b6f2b128e6ca
CaptainNibblesworth <josh.innis@decentralclub.com>
2021-07-24 22:37:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-esueb1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-esueb1)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+//Angular
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//NgRx
+import { StoreModule } from '@ngrx/store';
+
+//Components
+import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+//Reducers
+import { counterReducer } from './counter.reducer';
+
+@NgModule({
+  declarations: [AppComponent, MyCounterComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      count: counterReducer
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
